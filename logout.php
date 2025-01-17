ca8d1f0c8c9022e3e749b72526496fa0262537bf
LeventeGitHUB2024 <159180492+LeventeGitHUB2024@users.noreply.github.com>
2025-01-17 13:53:27 +0100

    apróbb fixek, designok

--- logout.php
+++ logout.php
@@ -7,8 +7,8 @@ $rememberMeSet = isset($_COOKIE['remember_me']) && isset($_COOKIE['remember_user
 // Ha a felhasználó be van jelentkezve, töröljük a tokent az adatbázisból
 if (isset($_SESSION['user_email'])) {
     $pdo = db();
-    $stmt = $pdo->prepare("UPDATE players_pyr SET remember_token = NULL WHERE email = :email");
-    $stmt->execute(['email' => $_SESSION['user_email']]);
+    // $stmt = $pdo->prepare("UPDATE players_pyr SET remember_token = NULL WHERE email = :email");
+    //$stmt->execute(['email' => $_SESSION['user_email']]); ezen még finomítani kell.  
 }
 
 // Csak akkor töröljük a cookie-kat, ha nincs "Remember Me" funkcióhoz kötve
