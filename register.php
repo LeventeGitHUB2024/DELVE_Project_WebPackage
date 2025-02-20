515f51f8c8a919a961d6b934038adbd2d01c77ed
Levente's_GitHub <levente3476@gmail.com>
2025-02-20 10:44:05 +0100

    email validation polish and improvement

--- register.php
+++ register.php
@@ -14,16 +14,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $errors[] = "The username can only consist of alphanumeric characters and underscores with the minimum length of at least 6 characters and a maximum of 20 characters!";
     }
 
+    // Listát alkothatsz az ismert trashmail szolgáltatókról
+    $trashmail_domains = [
+    '10minutemail.com', 'guerrillamail.com', 'sharklasers.com', 'getairmail.com', 'spamgourmet.com', 'temp-mail.org', 'discard.email', 'getnada.com', 'protonmail.com', 'mailnesia.com', 'yopmail.com', 'dropmail.me', 'mailcatch.com', 'tempmailaddress.com', 'moakt.com', 'onboxed.com', 'dispostable.com', 'tempmailaddress.net', 'throwawaymail.net', 'mail-temp.com', 'jetable.org', 'guerrillamail.org', 'fakeinbox.net', 'spamex.com', 'mailinator.org', 'mailfa.tk', 'quickfakes.com', 'spambog.com', 'spamsurf.com', 'inboxbear.com', 'disposemail.com', 'trashymail.com', 'spamcatcher.com', 'mailtome.com', 'spamfree24.com', 'discardmail.com', 'slopsbox.com', 'yourmail.fun', 'byom.de', 'throwawaymailbox.com', 'tempinbox.com', 'nomail.ga', 'lastmail.com', 'privatmail.com', 'inboxdrop.com', 'dmail.org', 'instant-mail.net', 'spamsave.com', 'onlyonetime.com', 'onetimeemail.com', 'gettempmail.com', 'tmailbox.com', 'noemail.ga', 'fakee-mail.com', 'itismymail.com', 'temporarymail.org', 'safe-mail.net', 'takeemail.com', 'newtempmail.com', 'maildrop.cc', 'tempmail.org', 'fakemail.org', 'tempinbox.net', 'tempmail.io', 'disposablemail.org', 'throwawayemail.com', 'spamfilter.com', 'nospam.email', 'quicktempmail.com', 'tempinbox.co', 'dodgeit.com', 'spambob.com', 'yopmail.org', 'onetimeemailaddress.com', 'gettempmail.net', 'nomail.com', 'tempxmail.com', 'fakemailbox.net', 'webmailgenerator.com', 'tempmail.us', 'throwawayemail.net', 'yourtempemail.com', 'inbox4.me', 'spamblocker.com', 'fakeaddress.com', 'getafakeemail.com', 'spammymail.com', 'tmail.co', 'notmail.com', 'postinbox.com', 'temporaryemail.me', 'thrownmail.com', 'tempmailr.com', 'foreveremail.com', 'junkmailbox.com', 'fake-mailbox.net', 'tempmailbox.com', 'emptyemail.com', 'spamali.com', 'trashmail.de', 'mail-dump.com', 'temp-email.com', 'discardableemail.com', 'mailcatcher.com', 'inboxgenerator.com', 'spamenvoy.com'
+    ];
+
+
     // Email validálása
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = "Please enter a valid e-mail adress!";
+        $errors[] = "The format of this email adress is not valid or not allowed.";
+    }else{
+        // Email domain lekérése
+        $email_domain = substr(strrchr($email, "@"), 1); // Kivágja a domain-t az email cím végéből
+
+        // Ellenőrizze, hogy a domain szerepel-e a trashmail listában
+        if (in_array($email_domain, $trashmail_domains)) {
+            $errors[] = "The email address is from a disposable email provider. Please use a valid email address.";
+        }
+
+        // DNS ellenőrzés: megnézi, hogy létezik-e MX rekord a domainhez
+        if (!checkdnsrr($email_domain, "MX")) {
+            $errors[] = "The domain of the email address does not exist, please use an existing and valid email address.";
+        }
     }
-
-    /*//Enyhébb email validálás
-    if (!preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/', $email)){
-        $errors[] = "Please enter a valid e-mail adress!";
-    }*/
-
 
     // Jelszó validálása
     if ($password !== $password2) { 
