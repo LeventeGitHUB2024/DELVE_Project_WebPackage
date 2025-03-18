39d8020955053be9eb0f24b039d61d2c860b7a97
LeventeGitHUB2024 <159180492+LeventeGitHUB2024@users.noreply.github.com>
2025-03-18 13:20:10 +0100

    more small fixes save.php

--- saves.php
+++ saves.php
@@ -201,7 +201,7 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
             <div class="bg-secondary rounded-top p-4">
                 <div class="row">
                     <div class="col-12 col-sm-6 text-center text-sm-start">
-                        &copy; <a href="#">DELVE UCP</a>, All Right Reserved.
+                        &copy; <strong>DELVE-PCG</strong>, All Right Reserved.
                     </div>
                     <div class="col-12 col-sm-6 text-center text-sm-end">
                         Designed By Szabó Levente</a>
@@ -214,9 +214,6 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
     </div>
     <!-- Content End -->
 
-
-    <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top mb-5"><i class="bi bi-arrow-up"></i></a>
     </div>
 
     <!-- JavaScript Libraries -->
